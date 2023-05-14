f2150494537fa06745eb17db2109f6991fe3de68
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-05-14 17:44:11 +0530

    working

--- app.js
+++ app.js
@@ -14,11 +14,12 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(statementRoutes);
 
 app.get('/', (req, res, next) => {
   res.send(<h1>Working till here...</h1>)
 });
+
+// app.use(statementRoutes);
 
 // Page Not Found handler
 app.use((err, req, res, next) => {
