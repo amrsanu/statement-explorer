06e8897f5f0a016c61870454038723342fc1d545
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-05-14 14:06:21 +0530

    error catcher

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(statementRoutes);
 
 // Page Not Found handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.use(errorController.get404);
 
 app.listen(3000, () => {
