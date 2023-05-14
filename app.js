30b16fe0425221d48e7ec8129d218fc42fb9f79e
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-05-14 14:58:52 +0530

    test3

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res, next) => {
-  res.send("<h1>Working till here...</h1>")
+  res.render('statement/index', {
+    pageTitle: 'Home',
+    path: '/',
+  });
 });
 
 // app.use(statementRoutes);
