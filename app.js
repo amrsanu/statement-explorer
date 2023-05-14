49cf3446233bb1538e60e0105f4041907c53b44c
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-05-14 13:50:42 +0530

    view fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const statementRoutes = require('./routes/statement');
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', './views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(statementRoutes);
