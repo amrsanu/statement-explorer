a2cfa6355cc0c7ac5e7bba6835808d0a91fbb1d3
Amrendra Pratap Singh <amrsanu@gmail.com>
2023-05-14 18:55:37 +0530

    added dummy statement page

--- controllers/statement.js
+++ controllers/statement.js
@@ -5,6 +5,37 @@ exports.getIndex = (req, res, next) => {
   });
 };
 
+exports.getStatement = (req, res, next) => {
+  res.render('statement/statement', {
+    min_date: '2020-01-01', 
+    max_date: '2023-12-31', 
+    credit_or_debit_option: 'All', 
+    expense_type_option: 'All',
+    credit_or_debit: ['All', 'Credit', 'Debit'], 
+    expense_type: ['All', 'Food', 'Transportation', 'Entertainment', 'Shopping', 'Others'], 
+    statement_table: ''
+  });
+};
+
+exports.postStatement = (req, res, next) => {
+  res.render('statement/statement', { 
+    min_date: '2020-01-01', 
+    max_date: '2023-12-31', 
+    credit_or_debit_option: 'All', 
+      expense_type_option: 'All',
+    credit_or_debit: ['All', 'Credit', 'Debit'], 
+    expense_type: ['All', 'Food', 'Transportation', 'Entertainment', 'Shopping', 'Others'], 
+    statement_table: "statement_table"
+  });
+};
+
+// exports.getUpdateStatement = (req, res, next) => {
+//   res.render('statement/update-statement', {
+//     pageTitle: 'Update Statement',
+//     path: '/update-statement',
+//   });
+// };
+
 exports.getHelp = (req, res, next) => {
   res.render('statement/help', {
     pageTitle: 'Help',
--- routes/statement.js
+++ routes/statement.js
@@ -6,6 +6,12 @@ const statementController = require('../controllers/statement');
 const router = express.Router();
 
 router.get('/', statementController.getIndex);
+
+router.get('/bank-statement', statementController.getStatement);
+router.post('/bank-statement', statementController.postStatement);
+
+// router.get('/update-statement', statementController.getUpdateStatement);
+
 router.get('/help', statementController.getHelp);
 
 module.exports = router;
